11b47037c4ac7284aa9621db688edb591c4e7c57
Chris Fife <ccfife@amazon.com>
2019-08-07 08:47:02 -0700

    changed website path

--- lib/staticwebsite-stack.ts
+++ lib/staticwebsite-stack.ts
@@ -93,7 +93,7 @@ export class StaticwebsiteStack extends cdk.Stack {
                 cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS
             }
           ],
-          originPath: '/static_website/sonar-master',
+          originPath: '/web/static',
         }
       ]
     })
